97e472e9c17b40109fce17c01279d82246b56241
Miguel <admorsus3@gmail.com>
2019-11-13 18:44:00 +0100

    Test src

--- src/Test/testRoman.java
+++ src/Test/testRoman.java
@@ -4,11 +4,11 @@ import RomanNumbers.Roman;
 
 public class testRoman {
 	public static void main (String[] args) {
-		System.out.println( Roman.toRoman	( 1931 ) );
-		System.out.println( Roman.toInteger ( "CDLXXVI" ) );
-		System.out.println( Roman.toRoman( 689 ) );
-		System.out.println( Roman.toInteger( "MCMXCIX" ) );
-		System.out.println( Roman.toRoman( 2019 ) );
-		System.out.println( Roman.toInteger( "DCLXXXIX" ) );
+		System.out.println(Roman.toRoman(1931));
+		System.out.println(Roman.toInteger("CDLXXVI"));
+		System.out.println(Roman.toRoman(689));
+		System.out.println(Roman.toInteger("MCMXCIX"));
+		System.out.println(Roman.toRoman(2019));
+		System.out.println(Roman.toInteger("DCLXXXIX"));
 	}
 }
